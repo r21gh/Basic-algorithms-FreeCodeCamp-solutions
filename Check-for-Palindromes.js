b9d45fcc84d1c711871afe85a39aee29f8833037
Reza Ghanbari <rezapython@gmail.com>
2016-08-10 11:23:22 +0430

    Create Check-for-Palindromes.js

---
+++ Check-for-Palindromes.js
@@ -0,0 +1,10 @@
+function palindrome(str) {
+  
+  if (str.replace(/[\W_]/g, '').toLowerCase() === str.replace(/[\W_]/g, '').toLowerCase().split("").reverse().join("")) {
+    return true;
+  }
+  
+  else {
+    return false;
+  }
+}
